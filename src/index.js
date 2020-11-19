d00e92ff59bd92d7d6544545388fdff475f598b8
Chris Laughlin <bluntm@gmail.com>
2020-11-19 23:06:34 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<div class="avatar">
+  <img src="https://randomuser.me/api/portraits/lego/1.jpg"/>
+  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="enable-background:new -580 439 577.9 194;"
+     xml:space="preserve">  
+    <circle cx="50" cy="50" r="40" />
+  </svg>
+</div>
+`;
