904d179b5f4a1aceb2314a7027321aef37ce6a3e
Chris Laughlin <bluntm@gmail.com>
2020-11-20 00:04:20 +0000

    adding story

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<div class="avatar">
-  <img src="https://randomuser.me/api/portraits/lego/1.jpg"/>
-  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="enable-background:new -580 439 577.9 194;"
-     xml:space="preserve">  
-    <circle cx="50" cy="50" r="40" />
-  </svg>
+<div>
+  <div class="avatar">
+    <img src="https://randomuser.me/api/portraits/lego/1.jpg"/>
+    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="enable-background:new -580 439 577.9 194;"
+      xml:space="preserve">  
+      <circle cx="50" cy="50" r="40" />
+    </svg>
+  </div>
+  <div class="gif">
+    <div class="close">X</div>
+    <iframe src="https://giphy.com/embed/tL3Nkk58oeWEo" width="480" height="219" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+  </div>
 </div>
 `;
+
+//Open story 
+document.querySelector('.avatar').addEventListener('click', evt => {
+  document.querySelector('.gif').classList.add('show');
+})
+
+// Close story 
+document.querySelector('.close').addEventListener('click', evt => {
+  document.querySelector('.gif').classList.remove('show');
+})
+
