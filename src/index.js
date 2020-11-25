2dcea7e06b3281b40962d42e34460b7c97bc8606
Chris Laughlin <chris_laughlin@rapid7.com>
2020-11-25 20:10:50 +0000

    changes

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
 import "./styles.css";
-
 document.getElementById("app").innerHTML = `
 <div>
   <div class="avatar">
     <img src="https://randomuser.me/api/portraits/lego/1.jpg"/>
-    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="enable-background:new -580 439 577.9 194;"
-      xml:space="preserve">  
-      <circle cx="50" cy="50" r="40" />
-    </svg>
   </div>
   <div class="gif">
     <div class="progress-line"></div>
-    <div class="progress-line-progression"></div>
+    <div class="progress-line-progress"></div>
     <div class="close">X</div>
-    <iframe src="https://giphy.com/embed/tL3Nkk58oeWEo" width="480" height="219" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+    <iframe src="https://giphy.com/embed/SRlNw1Cd86xmFpX4i0" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
   </div>
 </div>
 `;
 
-//Open story
-document.querySelector(".avatar").addEventListener("click", (evt) => {
-  document.querySelector(".gif").classList.add("show");
-  setTimeout(() => {
-    document
-      .querySelector(".progress-line-progression")
-      .classList.add("progress");
-  }, 0);
-});
+document
+    .querySelector('img')
+    .addEventListener('click', event => {
+        document
+            .querySelector('.gif')
+            .classList.add('show');
+        setTimeout(() => {
+            document
+                .querySelector('.progress-line-progress')
+                .classList.add('growing');
+        }, 0)
+    })
 
-// Close story
-document.querySelector(".close").addEventListener("click", (evt) => {
-  document.querySelector(".gif").classList.remove("show");
-  document
-    .querySelector(".progress-line-progression")
-    .classList.remove("progress");
-});
+document
+    .querySelector('.close')
+    .addEventListener('click', () => {
+        document
+            .querySelector('.gif')
+            .classList.remove('show');
+        document
+            .querySelector('.progress-line-progress')
+            .classList.remove('growing');
+    })
